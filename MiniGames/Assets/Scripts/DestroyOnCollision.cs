a3155743698d09f4b112e2d521dadd4946b4d742
cpowelll <2cedarsabby@gmail.com>
2021-06-14 22:21:49 -0700

    General Map Fixes
    Moving GameObjects locations. Added comments to Destroy on Collision script comonent

---
+++ MiniGames/Assets/Scripts/DestroyOnCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+If the tagName is not specified or the colliding gameobject has
+a tag that matches the inputted tagName parameter, destroy colliding gameobject
+*/
+public class DestroyOnCollision : MonoBehaviour
+{
+    public string tagName;
+
+    void OnTriggerEnter2D(Collider2D other){
+      if(tagName == other.gameObject.tag || tagName == ""){
+        Destroy(other.gameObject);
+      }
+    }
+
+}
